9d04671273051e9f728a04a12197226dbada03ac
Todd Utterback <todd.j.utterback@gmail.com>
2016-03-04 13:21:36 -0700

    Routes for Sellers

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -1,10 +1,46 @@
 class SellersController < ApplicationController
+
   def index
+    @sellers = Seller.all
   end
 
   def show
+     @seller = Seller.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    @seller = Seller.find(params[:id])
+    if @seller.update(seller_params)
+      redirect_to seller_path(@seller)
+    else
+      render :edit
+    end
   end
 
   def new
+    @seller = Seller.new
   end
+
+  def create
+    @seller = Seller.new(seller_params)
+    if @seller.save
+      redirect_to sellers_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @seller.destroy
+    redirect_to sellers_path
+  end
+
+  private
+    def seller_params
+      params.require(:seller).permit(:name, :phone_number, :email)
+    end
 end
+
